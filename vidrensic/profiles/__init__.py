8ab32d6a4c175a8eda19689a8f74d2d1671ca2fc
imed@CybrexTech <76208805+imedkablavi@users.noreply.github.com>
2026-08-21 02:04:35 +0300

    Expose variant profile registry

---
+++ vidrensic/profiles/__init__.py
@@ -0,0 +1,13 @@
+from vidrensic.profiles.registry import (
+    ProfileRegistry,
+    VariantProfile,
+    builtin_profiles,
+    default_profile_registry,
+)
+
+__all__ = [
+    "ProfileRegistry",
+    "VariantProfile",
+    "builtin_profiles",
+    "default_profile_registry",
+]
